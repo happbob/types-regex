d68a7b313b0969d2a4b9ca324f9605bd5cba2a8b
devbob <limms1217@naver.com>
2021-06-30 15:37:46 +0900

    21.06.30 add domain regex

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 const emailRegex = /^([\w_\.\-\+])+\@([\w\-]+\.)+([\w]{2,10})+$/;
 const phoneRegex = /^\d{11}$/;
+const domainRegex = {
+    dom : /(http(s)?:\/\/|www.)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}/gi,
+    par : /(http(s)?:\/\/|www.)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}([\/a-z0-9-%#?&=\w])+(\.[a-z0-9]{2,4}(\?[\/a-z0-9-%#?&=\w]+)*)*/gi
+  }
 const phoneFormChange = function (phone:string){
     if(phone.match( /^\d{11}$/ )){
         phone = phone.substr(0,3) + "-" + phone.substr(3, 4) + "-" + phone.substr(7,4);
@@ -8,4 +12,4 @@ const phoneFormChange = function (phone:string){
         return '';
     }
 }
-export {emailRegex, phoneRegex, phoneFormChange};
+export {emailRegex, phoneRegex, domainRegex, phoneFormChange};
